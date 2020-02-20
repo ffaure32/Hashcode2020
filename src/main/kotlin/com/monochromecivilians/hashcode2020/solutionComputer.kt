c1bc6e96ec5273d7fd7bbd0b9b96440e252574e5
Yoann Bourdex <yoann.bourdex@zenika.com>
2020-02-20 19:50:03 +0100

    scan

--- src/main/kotlin/com/monochromecivilians/hashcode2020/solutionComputer.kt
+++ src/main/kotlin/com/monochromecivilians/hashcode2020/solutionComputer.kt
@@ -3,8 +3,20 @@ package com.monochromecivilians.hashcode2020
 const val fileName = "a_example"
 
 fun resolve(input: ParsedInput): Output {
-    val libraries = input.libraries
-    for (library in libraries) {
-        library
+    var libraries = input.libraries
+    var deadline = input.nbDays
+    var library = findLibraryToScan(deadline, libraries)
+    while(library != null) {
+        scan(library)
+        libraries = input.libraries.filter { it != library }
+        deadline -= library.recordTime
+        library = findLibraryToScan(deadline, libraries)
     }
+}
+
+fun findLibraryToScan(remaningTime: Int, libraries: List<Library>): Library? =
+        libraries.find { it.recordTime < remaningTime }
+
+fun scan(library: Library) {
+    
 }
