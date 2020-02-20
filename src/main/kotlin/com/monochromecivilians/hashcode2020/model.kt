540fa1e0744798089b3601d67ba2c9bcf5f806a3
Yoann Bourdex <yoann.bourdex@zenika.com>
2020-02-20 19:57:08 +0100

    toto

--- src/main/kotlin/com/monochromecivilians/hashcode2020/model.kt
+++ src/main/kotlin/com/monochromecivilians/hashcode2020/model.kt
@@ -1,16 +1,16 @@
 package com.monochromecivilians.hashcode2020
 
-data class Book(val id : Int, val score : Int) {}
+data class Book(val id: Int, val score: Int) {}
 
-data class Library(val id : Int, val recordTime : Int, val bookIds  : List<Book>, val scansPerDay : Int)
+data class Library(val id: Int, val recordTime: Int, val bookIds: List<Book>, val scansPerDay: Int)
 
-data class Output(val librariesCount : Int, val libraries : List<Library>, val bookScannedCount : Int) {
+data class Output(var librariesCount: Int = 0, var libraries: MutableList<Library> = mutableListOf(), var bookScannedCount: Int = 0) {
     fun generateOutput(): List<String> {
         val output = mutableListOf<String>()
         output.add(librariesCount.toString())
-        libraries.forEach {
-            output.add(it.id.toString() + " "+it.bookIds.size)
-            output.add(it.bookIds.map { it.id }.joinToString { " "})
+        libraries.forEach { library ->
+            output.add(library.id.toString() + " " + library.bookIds.size)
+            output.add(library.bookIds.map { it.id }.joinToString { " " })
         }
         return output
     }
--- src/main/kotlin/com/monochromecivilians/hashcode2020/solutionComputer.kt
+++ src/main/kotlin/com/monochromecivilians/hashcode2020/solutionComputer.kt
@@ -3,20 +3,24 @@ package com.monochromecivilians.hashcode2020
 const val fileName = "a_example"
 
 fun resolve(input: ParsedInput): Output {
+    var output = Output()
     var libraries = input.libraries
     var deadline = input.nbDays
     var library = findLibraryToScan(deadline, libraries)
-    while(library != null) {
-        scan(library)
+    while (library != null) {
+        scan(library, output)
         libraries = input.libraries.filter { it != library }
         deadline -= library.recordTime
         library = findLibraryToScan(deadline, libraries)
     }
+    return output
 }
 
 fun findLibraryToScan(remaningTime: Int, libraries: List<Library>): Library? =
         libraries.find { it.recordTime < remaningTime }
 
-fun scan(library: Library) {
-    
+fun scan(library: Library, output: Output) {
+    output.librariesCount++
+    output.bookScannedCount += library.bookIds.size
+    output.libraries.add(library)
 }
