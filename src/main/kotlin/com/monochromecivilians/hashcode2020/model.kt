5ae267ff368deb9905e902a5c67b9ac7b5bfc20f
ffaure <frederic.faure@ext.cdiscount.com>
2020-02-20 19:03:20 +0100

    data model

--- src/main/kotlin/com/monochromecivilians/hashcode2020/model.kt
+++ src/main/kotlin/com/monochromecivilians/hashcode2020/model.kt
@@ -2,3 +2,4 @@ package com.monochromecivilians.hashcode2020
 
 data class Book(val id : Int, val score : Int) {}
 
+data class Library(val id : Int, val recordTime : Int, val bookIds  : List<Int>, val scansPerDay : Int)
