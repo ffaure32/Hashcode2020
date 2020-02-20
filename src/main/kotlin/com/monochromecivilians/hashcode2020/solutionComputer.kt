884c5f6645327c5dbb83a0dd3e76e227dd3cffc5
Yoann Bourdex <yoann.bourdex@zenika.com>
2020-02-20 19:35:57 +0100

    resolve

--- src/main/kotlin/com/monochromecivilians/hashcode2020/solutionComputer.kt
+++ src/main/kotlin/com/monochromecivilians/hashcode2020/solutionComputer.kt
@@ -2,6 +2,9 @@ package com.monochromecivilians.hashcode2020
 
 const val fileName = "a_example"
 
-fun resolve(input: ParsedInput): List<String> {
-    
+fun resolve(input: ParsedInput): Output {
+    val libraries = input.libraries
+    for (library in libraries) {
+        library
+    }
 }
